e5d120bc22cd56b600a19d77074f33f16d84fb81
Adeeb Ismail <51217487+AdeebIsmail@users.noreply.github.com>
2023-03-29 20:55:38 -0500

    foundations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import TypeWriterEffect from "react-typewriter-effect";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Navbar from "./components/navbar";
+import Typography from "@mui/material/Typography";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Chip from "@mui/material/Chip";
+import EmailIcon from "@mui/icons-material/Email";
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#222222",
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        {/* <Navbar></Navbar> */}
+        <Grid
+          container
+          // spacing={1.5}
+          rowSpacing={1.5}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "100vh" }}
         >
-          Learn React
-        </a>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </header>
-    </div>
+          <Grid item xs={3}>
+            <TypeWriterEffect
+              textStyle={{ fontFamily: "Roboto" }}
+              startDelay={0}
+              cursorColor="white"
+              text="Adeeb Ismail"
+              typeSpeed={100}
+            />
+            {/* <Typography variant="h4" component="h2">
+              Adeeb Ismail
+            </Typography> */}
+          </Grid>
+          {/* <Grid item xs={3} spacing={7}>
+            <Chip label="Clickable" variant="outlined" />{" "}
+            <Chip label="Clickable" variant="outlined" />{" "}
+          </Grid> */}
+
+          <Grid item xs={3}>
+            {" "}
+            <Chip label="ReFinance" variant="outlined" />{" "}
+            <Chip label="Giftin" variant="outlined" />{" "}
+            <Chip label="Empath" variant="outlined" />{" "}
+            <Chip label="OTHSCS" variant="outlined" />
+          </Grid>
+          <Grid
+            item
+            container
+            alignItems="center"
+            justifyContent="center"
+            spacing={6}
+          >
+            <Grid item xs={0.5}>
+              <GitHubIcon></GitHubIcon>
+            </Grid>
+            {/* <Grid item xs={0.5}>
+              <EmailIcon></EmailIcon>
+            </Grid> */}
+            <Grid item xs={0.5}>
+              <LinkedInIcon></LinkedInIcon>
+            </Grid>
+          </Grid>
+        </Grid>
+      </div>
+    </ThemeProvider>
   );
 }
 
