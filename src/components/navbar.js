661d5f50e2428adb3184fe6a79e22a594cefc700
Adeeb Ismail <51217487+AdeebIsmail@users.noreply.github.com>
2023-03-31 18:12:19 -0500

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -37,10 +37,6 @@ export default function ButtonAppBar() {
             <Button variant="text" color="inherit" style={{ borderRadius: 0 }}>
               Resume
             </Button>
-            <Divider orientation="vertical" flexItem></Divider>
-            <Button variant="text" color="inherit" style={{ borderRadius: 0 }}>
-              Course Work
-            </Button>
           </Grid>
         </Toolbar>
       </AppBar>
