5f031d9951329609f5b30c554a66899a5e14660f
Adeeb Ismail <51217487+AdeebIsmail@users.noreply.github.com>
2023-07-21 04:46:13 -0500

    code

---
+++ src/pages/home.js
@@ -0,0 +1,158 @@
+import "../App.css";
+import React from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import TypeWriterEffect from "react-typewriter-effect";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Navbar from "../components/navbar";
+import Typography from "@mui/material/Typography";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Chip from "@mui/material/Chip";
+import IconButton from "@mui/material/IconButton";
+import ArticleIcon from "@mui/icons-material/Article";
+import pdf from "../components/resume.pdf";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#222222",
+    },
+  },
+});
+
+function Home() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <Navbar></Navbar>
+        <Grid
+          container
+          // spacing={1.5}
+          rowSpacing={1.5}
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "90vh" }}
+        >
+          <Grid item xs={3}>
+            <TypeWriterEffect
+              textStyle={{ fontFamily: "Roboto" }}
+              startDelay={0}
+              cursorColor="white"
+              text="Adeeb Ismail"
+              typeSpeed={100}
+            />
+            {/* <Typography variant="h4" component="h2">
+              Adeeb Ismail
+            </Typography> */}
+          </Grid>
+          {/* <Grid item xs={3} spacing={7}>
+            <Chip label="Clickable" variant="outlined" />{" "}
+            <Chip label="Clickable" variant="outlined" />{" "}
+          </Grid> */}
+
+          <Grid item xs={3}>
+            {" "}
+            <Chip
+              href="https://github.com/AdeebIsmail/ReFinance"
+              component="a"
+              label="ReFinance"
+              variant="outlined"
+              clickable
+              style={{ marginTop: 5 }}
+            />{" "}
+            <Chip
+              href="https://github.com/KshitijKapoor8/Giftin"
+              component="a"
+              label="Giftin"
+              variant="outlined"
+              clickable
+              style={{ marginTop: 5 }}
+            />{" "}
+            <Chip
+              component="a"
+              label="Empath"
+              variant="outlined"
+              href="https://youtu.be/Ax-Rc4KPydk"
+              clickable
+              style={{ marginTop: 5 }}
+            />{" "}
+            <Chip
+              href="https://github.com/AdeebIsmail/ImageMetaData"
+              component="a"
+              label="Image Metadata"
+              variant="outlined"
+              clickable
+              style={{ marginTop: 5 }}
+            />{" "}
+            <Chip
+              component="a"
+              href="https://github.com/SamrutGadde/OTHSCS"
+              label="OTHSCS"
+              variant="outlined"
+              clickable
+              style={{ marginTop: 5 }}
+            />{" "}
+          </Grid>
+
+          {/* <Grid item xs={0.5}>
+              <EmailIcon></EmailIcon> = {}
+            </Grid> */}
+          <Grid item xs={2} sm={1} md={1} lg={0} xl={0.5}>
+            <IconButton
+              href="https://github.com/AdeebIsmail/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GitHubIcon></GitHubIcon>
+            </IconButton>
+
+            <IconButton
+              href="https://www.linkedin.com/in/adeeb-ismail-a87762276/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedInIcon></LinkedInIcon>
+            </IconButton>
+            <IconButton href={pdf} target="_blank" rel="noopener noreferrer">
+              <ArticleIcon></ArticleIcon>
+            </IconButton>
+          </Grid>
+
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="flex-end"
+            spacing={0}
+            style={{
+              top: 0,
+              position: "absolute",
+            }}
+          >
+            <Navbar></Navbar>
+            {/* <Grid item xs md={2} sm={2} xl={1}>
+              <Typography variant="subtitle1" component="h2">
+                About me
+              </Typography>
+            </Grid>
+            <Grid item xs md={2} sm={2} xl={1}>
+              <Typography variant="subtitle1" component="h2">
+                Resume
+              </Typography>
+            </Grid> */}
+          </Grid>
+        </Grid>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default Home;
---
+++ src/pages/pictures.js
@@ -0,0 +1,79 @@
+import "../App.css";
+import { React, useState, useRef } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Grid } from "@mui/material";
+import { CoPresentSharp } from "@mui/icons-material";
+import picture1 from "../pictures/IMG_8806.jpg";
+import picture2 from "../pictures/IMG_3699.jpg";
+import picture3 from "../pictures/IMG_3739.jpg";
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#222222",
+    },
+  },
+});
+
+function Pictures() {
+  // const copyToClip = (id) => {
+  //   const url = window.location.href;
+  //   navigator.clipboard.writeText(url + id);
+  // };
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Grid
+        container
+        direction="column"
+        justifyContent="space-evenly"
+        alignItems="center"
+      >
+        <Card sx={{ maxWidth: 500, marginTop: "1em" }}>
+          <CardMedia sx={{ height: 400, width: 500 }} image={picture1} />
+          <CardContent>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ fontStyle: "italic" }}
+            >
+              “We wants it, we needs it. Must have the precious.” — Gollum
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{ maxWidth: 500, marginTop: "1em" }}>
+          <CardMedia sx={{ height: 400, width: 500 }} image={picture2} />
+          <CardContent>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ fontStyle: "italic" }}
+            >
+              whaaaattt????
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{ maxWidth: 500, marginTop: "1em" }}>
+          <CardMedia sx={{ height: 400, width: 500 }} image={picture3} />
+          <CardContent>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              sx={{ fontStyle: "italic" }}
+            >
+              In one ear and out the other
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </ThemeProvider>
+  );
+}
+export default Pictures;
