a9345101121aa3b8120052169d78c505aca6e55d
Adeeb Ismail <adeebi5412@protonmail.com>
2024-06-08 15:22:25 -0500

    Updated to Vite

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+// import reportWebVitals from "./reportWebVitals";
 // import ReactGA from "react-ga4";
 import { HashRouter } from "react-router-dom";
 // ReactGA.initialize("G-XCLQN5YK7J");
